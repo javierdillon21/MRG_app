91215553c6c3d6346a6fe5a800de170d6a700be8
javierdillon21 <javierdillon21@gmail.com>
2021-06-02 01:47:40 -0500

    update Jun2

---
+++ pages/devocionales.tsx
@@ -0,0 +1,19 @@
+export default function Devocionales() {
+  return (
+    <div className="absolute flex flex-col gap-2 w-screen h-full z-0">
+      <div className="relative flex h-1/3 border-b border-gray-300 justify-center">
+        <div className="absolute flex flex-col top-3/4 min-h-full w-4/5 bg-gray-300 z-60 justify-center items-center">
+          <div className="absolute flex w-3/5 bg-gray-600">
+            <p className="break-words text-justify">
+              holas ajedianidnailndnaodnaodnaoidejm paokdpoaedp adpoa epdjapedj
+              apijdpiea
+            </p>
+          </div>
+          <p className="flex w-1/5 h-full">
+            holas djliwdlanwdlinaodanwdjlnawndnawjdnawo
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,78 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="absolute flex flex-col gap-2 w-screen h-full">
+      <div className="flex h-3/5"></div>
+      <div
+        id="navegation_bar"
+        className="flex-1 grid grid-cols-3 grid-rows-2 h-14 px-2 bg-white border-gray-400 border-t w-full text-xs items-center justify-items-center text-center leading-3"
+      >
+        <Link href="/devocionales">
+          <button className="group flex flex-col h-full justify-center items-center gap-3">
+            <FontAwesomeIcon
+              icon="coffee"
+              className="fill-current text-gray-500 transform scale-250"
+            />
+            <p className="flex w-20 justify-center">Devocionales</p>
+          </button>
+        </Link>
+        <Link href="/devocionales">
+          <button className="group flex flex-col h-full justify-center items-center gap-3">
+            <FontAwesomeIcon
+              icon="sticky-note"
+              className="fill-current text-gray-500 transform scale-250"
+            />
+            <p className="flex w-20 justify-center">Anotaciones</p>
+          </button>
+        </Link>
+        <Link href="/devocionales">
+          <button className="group flex flex-col h-full justify-center items-center gap-3">
+            <FontAwesomeIcon
+              icon="pray"
+              className="fill-current text-gray-500 transform scale-250"
+            />
+            <p className="flex w-20 justify-center">Peticiones de oración</p>
+          </button>
+        </Link>
+        <Link href="/devocionales">
+          <button className="group flex flex-col h-full justify-center items-center gap-3">
+            <FontAwesomeIcon
+              icon="hand-holding-heart"
+              className="fill-current text-gray-500 transform scale-250"
+            />
+            <p className="flex w-20 justify-center">Donación</p>
+          </button>
+        </Link>
+        <Link href="/devocionales">
+          <button className="group flex flex-col h-full justify-center items-center gap-3">
+            <FontAwesomeIcon
+              icon="calendar-alt"
+              className="fill-current text-gray-500 transform scale-250"
+            />
+            <p className="flex w-20 justify-center">
+              Calendario de actividades
+            </p>
+          </button>
+        </Link>
+        <Link href="/devocionales">
+          <button className="group flex flex-col h-full justify-center items-center gap-3">
+            <FontAwesomeIcon
+              icon="grip-horizontal"
+              className="fill-current text-gray-500 transform scale-250"
+            />
+            <p className="flex w-20 justify-center">Más</p>
+          </button>
+        </Link>
+      </div>
+
+      {/* <div className="flex flex-col h-full w-full gap-4 px-4">
+        <div className="flex h-32 bg-gray-500 flex-shrink-0 ">
+          <p>DEVOCIONALES</p>
+        </div>
+      </div> 
+      href={`/${carrusel[posicion].idProyecto}`}*/}
+    </div>
+  );
+}
